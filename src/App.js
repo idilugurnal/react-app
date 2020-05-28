c2766f1e8685e7b2b9831d547487ed9b34344667
Idil Ugurnal <ugurnal15@itu.edu.tr>
2020-05-28 13:41:44 +0300

    redux added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import { createStore } from "redux";
+import Footer from "./Footer";
+
+const updateCityDatas = {
+  type: "UPDATE_CITY",
+  payload: "Tokyo",
+};
+
+function cityReducer(state = "Seoul", action) {
+  switch (action.type) {
+    case "UPDATE_CITY":
+      return action.payload;
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const firstName = "Idil";
+  const lastName = "Ugurnal";
+  const cities = ["Seoul", "London", "Paris", "Rome"];
+  const listItems = cities.map((city) => <li>{city}</li>);
+  const store = createStore(cityReducer);
+  console.log(store.getState());
+  store.dispatch(updateCityDatas);
+  console.log(store.getState());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <h1>Welcome to My Page!</h1>
+        <h2>{`${firstName} ${lastName}`}</h2>
+        <ul>{listItems}</ul>
+      </nav>
+      <main>
+        <p>This is where the main content will go!</p>
+      </main>
+      <Footer />
     </div>
   );
 }
